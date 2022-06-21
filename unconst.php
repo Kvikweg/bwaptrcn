e06b51888df4b46c38b9f88b784320df5d5da61a
Kvikweg <Gromawkas@gmail.com>
2022-06-21 12:42:37 +0300

    Create unconst.php

---
+++ unconst.php
@@ -0,0 +1,15 @@
+my page code 
+<!DOCTYPE html>
+<html>
+<body>
+<style>
+body {
+  background-image: url('/images/un_const.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style> 
+
+</body>
+</html>
